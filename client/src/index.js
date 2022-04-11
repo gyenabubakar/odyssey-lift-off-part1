795d930be4c3fca27686ee797f806da854297fe0
Gyen Abubakar <indehyde@gmail.com>
2022-04-11 11:39:26 +0000

    initialised ApolloClient and wrapped app root component with ApolloProvider

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import GlobalStyles from './styles';
 import Pages from './pages';
+import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
+
+const gqlClient = new ApolloClient({
+    uri: 'https://localhost:4000',
+    cache: new InMemoryCache()
+})
 
 ReactDOM.render(
-  <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <ApolloProvider client={gqlClient}>
+        <React.StrictMode>
+            <GlobalStyles/>
+            <Pages/>
+        </React.StrictMode>
+    </ApolloProvider>,
+    document.getElementById('root')
 );
