8fdc4dd7fe9ec86947a0bc48610d04f05f85b0dc
Gyen Abubakar <indehyde@gmail.com>
2022-04-11 11:40:38 +0000

    composed tracks query in tracks page

--- client/src/pages/tracks.js
+++ client/src/pages/tracks.js
@@ -1,12 +1,30 @@
 import React from 'react';
-import { Layout } from '../components';
+import {Layout} from '../components';
+import {gql} from "@apollo/client";
+
+const TRACKS = gql`
+    query getTracks {
+        tracksForHome {
+            id
+            title
+            thumbnail
+            length
+            modulesCount
+            author {
+                id
+                name
+                photo
+            }
+        }
+    }
+`
 
 /**
  * Tracks Page is the Catstronauts home page.
  * We display a grid of tracks fetched with useQuery with the TRACKS query
  */
 const Tracks = () => {
-  return <Layout grid> </Layout>;
+    return <Layout grid> </Layout>;
 };
 
 export default Tracks;
