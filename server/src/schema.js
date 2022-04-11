6465906813af25b78d4f0d79ce6053e7bac390db
Gyen Abubakar <indehyde@gmail.com>
2022-04-11 11:18:02 +0000

    moved schema stuff to schema.js file

---
+++ server/src/schema.js
@@ -0,0 +1,33 @@
+const {gql} = require('apollo-server');
+
+const typeDefs = gql`
+    type Query {
+        "Tracks list for home page."
+        tracksForHome: [Track!]!
+    }
+
+    "A track is a group of modules that teaches about a specific topic."
+    type Track {
+        id: ID!
+        "Title of a track."
+        title: String!
+        "Photo URL of a track."
+        thumbnail: String
+        "Approximate number of minutes modules take to complete."
+        length: Int
+        "Number of modules in a track."
+        modulesCount: Int
+        "User who created a track."
+        author: Author!
+    }
+
+    type Author {
+        id: ID!
+        "First and last names of author."
+        name: String!
+        "Author's photo URL."
+        photo: String
+    }
+`;
+
+module.exports = typeDefs;
