54e1510ad41611891294e19ebe024eaf0d8e0632
Gyen Abubakar <indehyde@gmail.com>
2022-04-11 10:35:29 +0000

    installed necessary packages and wrote GraphQL schema

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,33 @@
-//TODO
+const {gql} = require('apollo-server');
+
+const typeDefs = gql`
+    type Query {
+        "Tracks list for home page."
+        tracksForHome: [Track!]!
+    }
+
+    "A track is a group of modules that teaches about a specific topic."
+    type Track {
+        id: String!
+        "Title of a track."
+        title: String!
+        "Photo URL of a track."
+        thumbnail: String
+        "Approximate number of minutes modules take to complete."
+        length: Int
+        "Number of modules in a track."
+        modulesCount: Int
+        "User who created a track."
+        author: Author!
+    }
+
+    type Author {
+        id: String!
+        "First and last names of author."
+        name: String!
+        "Author's photo URL."
+        photo: String
+    }
+`;
+
+module.exports = typeDefs;
